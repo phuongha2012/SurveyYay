61c0fd46fc782fdf29d95b32888da5849fea976d
Hayley Ha <hami201289@him-mail.ch>
2018-07-17 18:09:40 +1200

    header conditional rendering

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
   render() {
     return (
       <nav>
@@ -9,9 +22,7 @@ class Header extends Component {
             SurveyYay
           </a>
           <ul className="right">
-            <li>
-              Login With Google
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +30,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
