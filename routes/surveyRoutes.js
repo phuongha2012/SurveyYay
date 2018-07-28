00102fbb526f12c477dd41241903d27b030159df
Hayley Ha <hami201289@him-mail.ch>
2018-07-28 19:57:15 +1200

    setting up mailer object

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const requireLogin = require('../middlewares/requireCredits');
+const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -12,8 +14,13 @@ module.exports = app => {
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email })),
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
+    });
+
+    //Send email here
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
   });
 };
